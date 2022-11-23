a6e827a8da1e452aa2edadb7627a4bc4edfb5862
pandanrv <f.pernincapet@gmail.com>
2022-11-23 21:05:12 +0200

    orderNow + mise en page

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
-const searchInput = document.querySelector("#search")
-const searchResult = document.querySelector(".table-results")
+const searchInput = document.querySelector("#search");
+const searchResult = document.querySelector(".table-results");
 
-let dataArray;  
+let dataArray;
 
-async function getUsers(){
+async function getUsers() {
+  const res = await fetch("https://randomuser.me/api/?nat=fr&results=50");
 
-    const res = await fetch("https://randomuser.me/api/?nat=fr&results=50")
+  const {results} = await res.json();
 
-    const { results } = await res.json()
+  dataArray = orderList(results);
 
-    dataArray = orderList(results)
+  createUserList(dataArray);
+}
 
-    createUserList(dataArray)
-} 
-
-getUsers()
+getUsers();
 
 function orderList(data) {
-
-    const orderedData = data.sort((a,b)  => {
-        if(a.name.last.toLowerCase() < b.name.last.toLowerCase()) {
-            return -1;
-        }
-        if(a.name.last.toLowerCase() > b.name.last.toLowerCase()) {
-            return 1;
-        }
-        return 0;
-    })
-    return orderedData;
+  const orderedData = data.sort((a, b) => {
+    if (a.name.last.toLowerCase() < b.name.last.toLowerCase()) {
+      return -1;
+    }
+    if (a.name.last.toLowerCase() > b.name.last.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
+  return orderedData;
 }
 
 function createUserList(userList) {
+  userList.forEach(user => {
+    const listItem = document.createElement("div");
+    listItem.setAttribute("classe", "table-item");
 
-    userList.forEach (user => {
-
-        const listItem = document.createElement("div");
-        listItem.setAttribute("classe", "table-item");
-
-        listItem.innerHTML = `<div class="table-item">
+    listItem.innerHTML = `<div class="table-item">
         <div class="container-img">
           <img src=${user.picture.medium} alt="">
           <p class="name">${user.name.last} ${user.name.first}</p>
         </div>
-        <p class="email">${user.email}</p>
-        <p class="data">${user.phone}</p>`
+        <select id="container-select">
+          <option value="">container</option>
+          <option value="">Dog</option>
+          <option value="">Cat</option>
+          <option value="">Hamster</option>
+          <option value="">Parrot</option>
+          <option value="">Spider</option>
+        </select>
+        <select id="quantity-select">
+          <option value="">quantity</option>
+          <option value="">0.100</option>
+          <option value="">0.200</option>
+          <option value="">0.500</option>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+          <option value="">5</option>
+        </select>
+        <button>confirm</button>`;
 
-        searchResult.appendChild(listItem);
-    })
+    searchResult.appendChild(listItem);
+  });
 }
 
-searchInput.addEventListener("input", filterData)
+searchInput.addEventListener("input", filterData);
 
 function filterData(e) {
+  searchResult.innerHTML = "";
 
-    searchResult.innerHTML = ""
+  const searchedString = e.target.value.toLowerCase();
 
-    const searchedString = e.target.value.toLowerCase();
+  const filteredArr = dataArray.filter(
+    el =>
+      el.name.first.toLowerCase().includes(searchedString) ||
+      el.name.last.toLowerCase().includes(searchedString)
+  );
 
-    const filteredArr = dataArray.filter(el => el.name.first.toLowerCase().includes(searchedString) || el.name.last.toLowerCase().includes(searchedString))
-
-    createUserList(filteredArr)
+  createUserList(filteredArr);
 }
